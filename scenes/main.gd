8a91fc8e0b0f20ef5703a56e37a80a4dfa9454f0
alecvd <14879968+AlecVD@users.noreply.github.com>
2023-03-06 22:50:44 -0800

    add mac export

--- scenes/main.gd
+++ scenes/main.gd
@@ -9,7 +9,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	##Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	if !OS.is_debug_build():
 		fast_close = false
