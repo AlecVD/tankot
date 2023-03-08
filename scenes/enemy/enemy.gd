813612acc9b353e7dac077eda0640361c7b303ca
alecvd <14879968+AlecVD@users.noreply.github.com>
2023-03-07 16:09:02 -0800

    add pointer

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,20 +1,16 @@
 extends RigidBody3D
 
-var bounces = 0
+@export var health = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
+#Big Mode
 func _on_body_entered(body):
-	#print(body.get_name())
-	# Destroy self and increment bounces
-	bounces -= 1
-	if(bounces < 0):
-		queue_free()
-	#print("hit "+ str(body.get_instance_id()))
-	pass # Replace with function body.
+	if(body.get_name() == "Bullet"):
+		print("hitb")
+
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,10 +2,12 @@ extends RigidBody3D
 
 @export var MOVE_SPEED = 50.0
 @export var TURN_SPEED = 150
+@export var bounces = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 @onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var bulletObj = preload("res://scenes/bullet/bullet.tscn")
+@onready var point =  get_node("Turret/Point")
 
 func _ready():
 	linear_damp = 10.0
@@ -33,10 +35,21 @@ func _physics_process(delta):
 	
 	##Fire Logic
 	if Input.is_action_just_pressed("fire"):
-		print("fire")
-		var bullet = bulletObj.instantiate()
-		bullet.rotation_degrees = rotation_degrees
-		bullet.transform = $Point.get_global_transform()
-		bullet.apply_central_impulse($Point.global_transform.basis.z * -10)
-		#get_node("root/main").add_child(bullet)
-		get_tree().get_root().get_node("Main").add_child(bullet)
+		fire()
+		
+	
+func fire():
+	##Fire Bullet from Point
+	#print("fire")b 
+	
+	var bullet = bulletObj.instantiate()
+	bullet.rotation_degrees = rotation_degrees
+	bullet.transform = point.get_global_transform()
+	bullet.bounces = bounces
+	bullet.apply_central_impulse(point.global_transform.basis.z * -10)
+	#get_node("root/main").add_child(bullet)
+	get_tree().get_root().get_node("Main").add_child(bullet)
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
